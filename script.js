350c28971e2734e61f3d96eef3307793388c11f7
Zennode <zennode@Zennodes-MacBook-Air.local>
2021-09-28 09:53:55 +0530

    Unwanted js code removed from script file

--- script.js
+++ script.js
@@ -1,47 +1,4 @@
-/*function validate()
-{
-if(document.getElementById("uname").value=="")
-{
-alert("Enter Your Mail");
-document.getElementById("uname").focus();
-return false;
-}
-var emailPat =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-			
-     var emailid=document.getElementById("uname").value;
 
-     var matchArray = emailid.match(emailPat);
-
-     if (matchArray == null)
-
-    {
-
-               alert("Your Email ID seems incorrect. Enter Correct Email ID.");
-
-               document.getElementById("uname").focus();
-
-               return false;
-
-	}
-	if(document.getElementById("pass").value=="")
-	{
-		alert(" Enter password");
-		document.getElementById("pass").focus();
-		return false;
-	}
-	if(document.getElementById("pass").value.length<8)
-
-    {
-
-    alert("Password must be of 8 or more characters");
-
-    document.getElementById("pass").focus();
-
-    return false;
-
-    }
-	return true;
-}*/	
 /*counter function using set and clear*/
 function counter()
 {
